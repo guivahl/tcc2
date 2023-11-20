6c100f2f8d469b290fa72612028de6527a04baaa
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2023-11-20 16:49:52 -0300

    feat: first working algorithm for pi calculation using elixir

--- actors/elixir/pi/lib/pi.ex
+++ actors/elixir/pi/lib/pi.ex
@@ -1,55 +1,67 @@
 defmodule Randomize do
   def start do
-    pid = spawn(__MODULE__, :loop, [])
+    pid = spawn(__MODULE__, :calculate, [])
     pid
   end
 
-  def loop do
+  def calculate do
     receive do
       {sender} ->
+        # Process.sleep(500)
         x = :rand.uniform()
         y = :rand.uniform()
 
-        is_inside = x * x + y * y <= 1.0
+        is_inside = (x * x + y * y) <= 1.0
+        # IO.puts("Esta dentro do circulo: #{is_inside}...")
 
         send(sender, {is_inside})
-        loop()
     end
   end
 end
 
 defmodule PI do
-  def start(total_points) do
-    pid = spawn(__MODULE__, :estimate, [total_points, total_points, 0])
+  def start(total_points, parent_pid) do
+    receiver_pid = spawn(__MODULE__, :receiver, [0, 1, total_points, parent_pid])
+    pid = spawn(__MODULE__, :estimate, [total_points, total_points, receiver_pid])
     pid
   end
 
-  def estimate(total_points, 0, inside_circle) do
-    estimated_pi = 4.0 * inside_circle / total_points
-    IO.puts("Valor estimado de PI para #{total_points} foi #{estimated_pi}")
+  def estimate(total_points, 0, receiver_pid) do :ok
   end
 
-  def estimate(total_points, num_points, inside_circle) do
+  def estimate(total_points, num_points, receiver_pid) do
     actor_pid = Randomize.start()
 
-    send(actor_pid, {self()})
+    send(actor_pid, {receiver_pid})
+    # IO.puts("Enviando mensagem: #{num_points}...")
+
+    estimate(total_points, num_points - 1, receiver_pid)
+  end
+
+  def receiver(points_inside_circle, count, total_points, parent_pid) when count == total_points do
+    estimated_pi = 4.0 * points_inside_circle / total_points
+
+    send(parent_pid, {:finish, estimated_pi})
+  end
+
+  def receiver(points_inside_circle, count, total_points, parent_pid) do
     receive do
       {true} ->
-        estimate(total_points, num_points - 1, inside_circle + 1)
+        receiver(points_inside_circle + 1, count + 1, total_points, parent_pid)
       {false} ->
-        estimate(total_points, num_points - 1, inside_circle)
+        receiver(points_inside_circle, count + 1, total_points, parent_pid)
+      {sender, :finish} ->
+        send(sender, points_inside_circle)
     end
   end
 end
 
-PI.start(1_000)
-# not working for values over 5k
+total_points = 1_000_000
+parent_pid = self()
 
-# pi = PI.start(1_000)
-# Process.exit(pi, :normal)
-# pi = PI.start(10_000)
-# PI.start(100_000)
-# PI.start(1_000_000)
-# PI.start(10_000_000)
-# PI.start(100_000_000)
-# PI.start(1_000_000_000)
+PI.start(total_points, parent_pid)
+
+receive do
+  {:finish, estimated_pi} ->
+    IO.puts("Valor estimado de PI para #{total_points} foi #{estimated_pi}")
+end
