75d369ebea42d6fa9be8f12f11ba76608dc6da6d
Guilherme dos Santos Vahl <gdsvahl@inf.ufpel.edu.br>
2024-03-03 11:17:05 -0300

    feat: adjustments image processor elixir

--- actors/elixir/image_processor/lib/image_processor.ex
+++ actors/elixir/image_processor/lib/image_processor.ex
@@ -4,7 +4,7 @@ defmodule ImageProcessor do
   def split_and_invert_image(input_path, split_width, split_height, initial_time) do
     parent_pid = self()
 
-    counter_pid = spawn(ImageProcessor, :counter, [0, split_width * split_height, split_width, split_height, initial_time, parent_pid])
+    counter_pid = spawn(ImageProcessor, :counter, [0, split_width * split_height, [], split_width, split_height, initial_time, parent_pid])
 
     input_image = Image.open!(input_path)
 
@@ -14,16 +14,18 @@ defmodule ImageProcessor do
     chunk_width = div(input_width, split_width)
     chunk_height = div(input_height, split_height)
 
+
     for i <- 1..(split_height), j <- 1..(split_width) do
       x_start = (j - 1) * chunk_width
       y_start = (i - 1) * chunk_height
 
-      order = (i * split_height) + j
+      order = ((j - 1)) + ((i - 1) * split_width)
 
       output_filename = "tmp/rotated_#{(i - 1)}_#{(j - 1)}.png"
       # IO.puts(output_filename)
-      order = (i * split_height) + j
-      spawn(ImageProcessor, :split_save, [input_image, x_start, y_start, chunk_width, chunk_height, output_filename, counter_pid])
+
+      spawn(ImageProcessor, :split_save, [input_image, order, x_start, y_start, chunk_width, chunk_height, output_filename, counter_pid])
+
     end
 
     receive do
@@ -31,39 +33,38 @@ defmodule ImageProcessor do
     end
   end
 
-  def split_save(input_image, x_start, y_start, chunk_width, chunk_height, output_filename, counter_pid) do
-    # IO.puts("teste")
+  def split_save(input_image, order, x_start, y_start, chunk_width, chunk_height, output_filename, counter_pid) do
+
     piece = Image.crop!(input_image, x_start, y_start, chunk_width, chunk_height)
 
     rotated_piece = Image.rotate!(piece, 180.0)
 
     with {:ok,_} <- Image.write(rotated_piece, output_filename) do
-      # IO.puts("count")
-      send(counter_pid, {:count})
+
+      send(counter_pid, {:count, {order, rotated_piece}})
     end
   end
 
-  def counter(count, max, split_width, split_height, initial_time, parent_pid) when count == max do
-    # IO.puts("max count")
-    spawn(ImageProcessor, :join, [split_width, split_height, initial_time, parent_pid])
+  def counter(count, max, images, split_width, split_height, initial_time, parent_pid) when count == max do
+    spawn(ImageProcessor, :join, [images, split_width, split_height, initial_time, parent_pid])
   end
 
-  def counter(count, max, split_width, split_height, initial_time, parent_pid) do
+  def counter(count, max, images, split_width, split_height, initial_time, parent_pid) do
     receive do
-      {:count} ->
-        # IO.puts(count)
-        counter(count + 1, max, split_width, split_height, initial_time, parent_pid)
+      {:count, {order, rotated_piece}} ->
+        counter(count + 1, max, images ++ [{order, rotated_piece}], split_width, split_height, initial_time, parent_pid)
     end
   end
 
-  def join(split_width, split_height, initial_time, parent_pid) do
-    images = for i <- (split_height)..1, j <- (split_width)..1 do
-      Image.open!("tmp/rotated_#{i - 1}_#{j - 1}.png")
-    end
+  def join(images, split_width, split_height, initial_time, parent_pid) do
+    sorted_images = images
+      |> Enum.sort(fn {index_a, _}, {index_b, _} -> index_a > index_b end)
+      |> Enum.map(fn {_, image} -> image end)
 
-    final_image = Image.join!(images, [{:across, split_width}])
+    final_image = Image.join!(sorted_images, [{:across, split_width}])
+
     output_path = "output_images/final_#{:os.system_time(:millisecond)}.png"
-    # IO.puts(output_path)
+
     with {:ok,_} <-  Image.write(final_image, output_path) do
       finish_time = Time.utc_now()
       time_execution = Time.diff(finish_time, initial_time, :millisecond)
--- actors/elixir/image_processor/lib/main.ex
+++ actors/elixir/image_processor/lib/main.ex
@@ -11,27 +11,6 @@ has_args = length(args) == 2
 split_width = if has_args, do: String.to_integer(Enum.at(args, 0)), else: default_split_width
 split_height = if has_args, do: String.to_integer(Enum.at(args, 1)), else: default_split_height
 
-# parent_pid = self()
-
 input_image_path = "image.png"
 
-# spawn(fn -> ImageProcessor.split_and_invert_image(input_image_path, split_width, split_height, initial_time) end)
-
 ImageProcessor.split_and_invert_image(input_image_path, split_width, split_height, initial_time)
-
-# receive do
-#   {:finish, output_path} ->
-#     finish_time = Time.utc_now()
-#     time_execution = Time.diff(finish_time, initial_time, :millisecond)
-
-#     n_actors = split_width * split_height
-
-#     csv_string = "#{n_actors},#{split_width},#{split_height},#{time_execution},#{output_path}\n"
-
-#     filename = "results.csv"
-
-#     {:ok, file} = File.open(filename, [:append])
-
-#     IO.write(file, csv_string)
-#     File.close(file)
-# end
